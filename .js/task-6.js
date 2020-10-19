9af9bf2fa2216373ecfeab5d0d2884bdaffe1502
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-19 18:44:00 +0300

    task-6

---
+++ .js/task-6.js
@@ -0,0 +1,25 @@
+// Напиши скрипт, який би при втраті фокуса на інпут,
+// перевіряв його вміст на правильну кількість символів.
+// Скільки символів має бути в інпут, вказується в його атрибуті data - length.
+// Якщо введена відповідна кількість, то border інпут стає зеленим, якщо неправильне - червоним.
+// Для додавання стилів, використовуй CSS - класи valid і invalid.
+
+
+const inputRef = document.querySelector('#validation-input');
+
+// inputRef.addEventListener('blur',)
+// inputRef.addEventListener('input',)
+
+const handleInputBlur = event => {
+    console.log(inputRef.value.length);
+    console.log(inputRef.dataset.length);
+    if (inputRef.value.length == inputRef.dataset.length) {
+        inputRef.classList.add('valid')
+        inputRef.classList.remove('invalid')
+    } else {
+        inputRef.classList.add('invalid')
+        inputRef.classList.remove('valid')
+    }
+}
+
+inputRef.addEventListener('blur', handleInputBlur)
