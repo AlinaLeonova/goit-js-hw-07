c913fd014bf333123c7f815a4d2a962d671bfdcf
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-20 13:31:38 +0300

    task-3 fixed

--- .js/task-3.js
+++ .js/task-3.js
@@ -1,14 +1,10 @@
 // Напиши скрипт для создания галлереи изображений по массиву данных.
-
 // В HTML есть список ul#gallery.
-
 {/* < ul id = "gallery" ></ul > */ }
 // Используй массив объектов images для создания тегов img вложенных в li.
 // Для создания разметки используй шаблонные строки и insertAdjacentHTML().
-
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css - классы.
-
 
 const listImages = document.querySelector('#gallery');
 const images = [
@@ -29,35 +25,7 @@ const images = [
     }
 ];
 
-
-// const fragment = document.createDocumentFragment();
-
 images.forEach(el => {
-
-    let message = `<li class="list.item"><img src=${el.url} alt=${el.alt} class="list-img"></li>`
+    let message = `<li><img src=${el.url} alt=${el.alt} class="list-img"></li>`
     listImages.insertAdjacentHTML('beforeend', message)
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // const createLi = document.createElement("li");
-    // const createImg = document.createElement("img");
-    // createLi.prepend(createImg);
-    // createImg.alt = item.alt;
-    // createImg.src = item.url;
-    // createImg.width = "400";
-    // createImg.height = "250";
-    // fragment.append(createLi);
