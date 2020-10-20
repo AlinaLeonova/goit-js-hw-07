1d47b7163095f02c2ccc4c772d21407faf3c5918
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-20 14:27:33 +0300

    task-1

--- .js/task-1.js
+++ .js/task-1.js
@@ -21,33 +21,3 @@ ulRef.forEach((el) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const ul = Array.from(document.querySelector('#categories').children);
-// for (let elem of ul) {
-//     let category = elem.firstElementChild.textContent;
-//     let quantityElem = elem.lastElementChild.children.length;
-//     console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
-// };
-
-
-
-
